437004fa2a28616dfb20cbcc7ff4a69d3a39aa38
Alex Bevilacqua <alexbevi@gmail.com>
2010-02-12 04:20:01 -0500

    article updating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,5 +22,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update_attributes(params[:article])
+      flash[:notice] = "Article Updated"
+      redirect_to({ :controller => 'knowledgebase', :action => 'index' })
+    else
+      render(:action => 'edit')
+    end    
+  end
 
 end
