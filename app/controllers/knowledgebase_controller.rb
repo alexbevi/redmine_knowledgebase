0a4f3f3328b4c15d9f08c82a3c003c08863cb04b
Alex Bevilacqua <alexbevi@gmail.com>
2010-02-15 18:33:20 -0500

    when trying to edit an inherited item (article, controller), check if the user is currently logged in. If not, render_403

--- app/controllers/knowledgebase_controller.rb
+++ app/controllers/knowledgebase_controller.rb
@@ -1,9 +1,19 @@
 class KnowledgebaseController < ApplicationController
   unloadable
   
+  before_filter :is_user_logged_in, :only => :edit
+  
   def index
 		@categories = Category.find(:all)
     @articles_newest = Article.find(:all)
   end
 
+protected
+  
+  def is_user_logged_in
+    if !User.current.logged?
+      render_403
+    end
+  end
+  
 end
