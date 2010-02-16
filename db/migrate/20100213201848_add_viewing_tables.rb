c389a002d26250c48bccb61a5550de93fa101353
Alex Bevilacqua <alexbevi@gmail.com>
2010-02-16 15:02:11 -0500

    forgot to require acts_as_viewed in the migration since we're not working directly with a plugin

--- db/migrate/20100213201848_add_viewing_tables.rb
+++ db/migrate/20100213201848_add_viewing_tables.rb
@@ -1,6 +1,8 @@
 class AddViewingTables < ActiveRecord::Migration
+  require 'acts_as_viewed'
+  
   def self.up
-    ActiveRecord::Base.create_viewings_table   
+    ActiveRecord::Base.create_viewings_table
   end
 
   def self.down    
