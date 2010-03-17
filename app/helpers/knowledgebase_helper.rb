2ac2bb7efd4cd4679d1a49f37622a0286e5006e8
Alex Bevilacqua <alexbevi@gmail.com>
2010-03-17 15:52:05 -0400

    differentiate "newest" and "last updated" items in the knowledgebase index

--- app/helpers/knowledgebase_helper.rb
+++ app/helpers/knowledgebase_helper.rb
@@ -3,4 +3,16 @@ module KnowledgebaseHelper
   def link_to_if_logged_in(name, options = {}, html_options = nil, *parameters_for_method_reference)
     link_to(name, options, html_options, *parameters_for_method_reference) if User.current.logged?
   end
+  
+  def format_article_summary(article, format)
+    output = nil
+    case format
+      when "newest"
+        output = "Created " + time_ago_in_words(article.created_at) + " ago in " + link_to(article.category.title, {:controller => 'categories', :action => 'show', :id => article.category.id})
+      when "updated"
+        output = "Updated " + time_ago_in_words(article.updated_at) + " ago"
+    end
+    
+    content_tag(:div, output, :class => "summary")
+  end
 end
