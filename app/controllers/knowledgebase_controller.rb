50221894bafa69c3db01311a8ae4db6c74123730
Alex Bevilacqua <alexbevi@gmail.com>
2010-03-17 15:10:10 -0400

    added some default messages for when no categories or articles exist (I know they're dumb, but it's a placeholder for now) hide the pipe seperator from the new category link when no categories exist

--- app/controllers/knowledgebase_controller.rb
+++ app/controllers/knowledgebase_controller.rb
@@ -4,6 +4,7 @@ class KnowledgebaseController < ApplicationController
   before_filter :is_user_logged_in, :only => :edit
   
   def index
+    @categories = Category.find(:all)
     @articles_newest  = Article.find(:all, :limit => 5, :order => 'created_at DESC')
     @articles_updated = Article.find(:all, :limit => 5, :order => 'updated_at DESC')
   end
