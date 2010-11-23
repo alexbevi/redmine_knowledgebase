1fde19eba54bac0b78ff2e511f34ae99341675ef
Alex Bevilacqua <alexbevi@gmail.com>
2010-11-23 15:49:23 -0500

    remove orphaned brace

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_knowledgebase do
 
   requires_redmine :version_or_higher => '0.8.0'
 
-  menu :top_menu, :knowledgebase, { :controller => 'knowledgebase', :action => 'index'}, :caption => 'Knowledgebase' }
+  menu :top_menu, :knowledgebase, { :controller => 'knowledgebase', :action => 'index'}, :caption => 'Knowledgebase'
 
 #  FIXME: uncomment when upgrading to redmine 1.0 or find any other
 #         way to hook into the searach engine
