1afe90f807b7a21ab7efa732dd7e366b36aee9fe
Alex Bevilacqua <alexbevi@gmail.com>
2010-11-25 15:31:43 -0500

    removed project dependency as it no longer seems to cause issues with acts_as_attachable

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,17 +1,15 @@
 class Article < ActiveRecord::Base
   unloadable
 
-  belongs_to :project
-  
   require 'acts_as_viewed'
   require 'acts_as_rated'
   require 'acts_as_taggable'
-  
+
   acts_as_viewed
   acts_as_rated :no_rater => true
   acts_as_taggable
   acts_as_attachable
-  
+
   acts_as_searchable :columns => ["kb_articles.title", "kb_articles.content"], :no_project_scope => true, :order_column => "kb_articles.id"
 
   acts_as_event :title => Proc.new {|o| "#{l(:label_title_articles)} ##{o.id}: #{o.title}" },
@@ -21,27 +19,23 @@ class Article < ActiveRecord::Base
                   :url => Proc.new {|o| {:controller => 'articles', :action => 'show', :id => nil, :article_id => o.id} }
 
   has_many :comments, :as => :commented, :dependent => :delete_all, :order => "created_on"
-  
-  validates_presence_of :title  
+
+  validates_presence_of :title
   validates_presence_of :category_id
-  
+
   belongs_to :category
   belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
-  
+
   def self.table_name() "kb_articles" end
-  
-  # This overrides the instance method in acts_as_attachable 
+
+  # This overrides the instance method in acts_as_attachable
   def attachments_visible?(user=User.current)
     true
   end
-  
+
   def attachments_deletable?(user=User.current)
     user.logged?
   end
-  
-  # acts_as_attachable requires a project association
-  def project
-    nil
-  end
 
 end
+
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,11 +2,10 @@ class Category < ActiveRecord::Base
   unloadable
   acts_as_nested_set :order => 'title'
 
-  belongs_to :project
+  validates_presence_of :title
 
-  validates_presence_of :title  
-  
   has_many :articles
-  
+
   def self.table_name() "kb_categories" end
 end
+
