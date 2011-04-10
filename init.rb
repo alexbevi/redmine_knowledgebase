240617e40a85e75bfbeffbda9e9a9c7ac629732d
Martin Denizet <martin.denizet@supinfo.com>
2011-04-10 22:24:29 +0800

    Implements global permissions for the controllers.

--- init.rb
+++ init.rb
@@ -13,8 +13,45 @@ Redmine::Plugin.register :redmine_knowledgebase do
 
   requires_redmine :version_or_higher => '0.8.0'
 
-  menu :top_menu, :knowledgebase, { :controller => 'knowledgebase', :action => 'index'}, :caption => 'Knowledgebase'
+  #Global permissions
+  project_module :knowledgebase do
+    permission :view_articles, {
+      :knowledgebase => :index,
+      :articles => [:show],:categories => [:index,:show]
+    }
+    permission :comment_and_rate_articles, {
+      :knowledgebase => :index,
+      :articles => [:show,:rate,:add_comment]
+    }
+    permission :create_articles, {
+      :knowledgebase => :index,
+      :articles => [:show,:new,:create,:add_attachment,:tagged,:preview]
+    }
+    permission :edit_articles, {
+      :knowledgebase => :index,
+      :articles => [:show,:edit,:update,:add_attachment,:tagged,:preview]
+    }
+    permission :manage_articles, {
+      :knowledgebase => :index,
+      :articles => [:show,:new,:create,:edit,:update,:destroy,:add_attachment,:preview,:destroy_comment,:tagged]
+    }
+    permission :manage_articles_comments, {
+      :knowledgebase => :index,
+      :articles => [:show,:add_comment,:destroy_comment]
+    }
+    permission :create_article_categories, {
+      :knowledgebase => :index,
+      :categories => [:index,:show,:new,:create]
+    }
+    permission :manage_article_categories, {
+      :knowledgebase => :index,
+      :categories => [:index,:show,:new,:create,:edit,:update,:delete]
+    }
+  end
+  
+  menu :top_menu, :knowledgebase, { :controller => 'knowledgebase', :action => 'index'}, :caption => 'Knowledgebase',:if =>  Proc.new {
+    User.current.allowed_to?({:controller => 'knowledgebase', :action => 'index'},nil, :global => true)
+  }
 
   Redmine::Search.available_search_types << 'articles'
 end
-
