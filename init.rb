17b820899e20e639d45c88795af956125ffeacb0
Alex Bevilacqua <alexbevi@gmail.com>
2011-05-10 11:27:57 -0400

    Update personal URL

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_knowledgebase do
   name        'Knowledgebase'
   author      'Alex Bevilacqua'
   description 'A plugin for Redmine that adds knowledgebase functionality'
-  url         'http://projects.alexbevi.com/projects/redmine-kb'
+  url         'http://alexbevi.com/projects/redmine-kb'
   author_url  'http://blog.alexbevi.com'
   version     '0.3.0'
 
