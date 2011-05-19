d1046a17c2501196ae705907a6b6f89f6c51d1ab
Alex Bevilacqua <alexbevi@gmail.com>
2011-05-19 15:31:12 -0400

    knowledgebase#index: filter out articles from the 'updated' list that haven't actually been updated yet

--- app/controllers/knowledgebase_controller.rb
+++ app/controllers/knowledgebase_controller.rb
@@ -7,7 +7,7 @@ class KnowledgebaseController < ApplicationController
   def index
     @categories = Category.find(:all)
     @articles_newest   = Article.find(:all, :limit => 5, :order => 'created_at DESC')
-    @articles_updated  = Article.find(:all, :limit => 5, :order => 'updated_at DESC')
+    @articles_updated  = Article.find(:all, :limit => 5, :conditions => ['created_at <> updated_at'], :order => 'updated_at DESC')
     @articles_toprated = Article.find(:all, :limit => 5, :order => 'updated_at DESC')
     @articles_popular  = Article.find(:all, :limit => 5, :order => 'updated_at DESC')
   end
