97f97c4b41692701d8d26d4456d22f85ce1f650a
Alex Bevilacqua <alexbevi@gmail.com>
2011-05-20 12:00:20 -0400

    force certain errors to recover by rendering Redmine's 404 page

--- app/controllers/knowledgebase_controller.rb
+++ app/controllers/knowledgebase_controller.rb
@@ -3,7 +3,10 @@ class KnowledgebaseController < ApplicationController
 
   #Authorize against global permissions defined in init.rb
   before_filter :authorize_global, :unless => :allow_anonymous_access?
-
+  
+  rescue_from ActionView::MissingTemplate, :with => :force_404
+  rescue_from ActiveRecord::RecordNotFound, :with => :force_404
+  
   def index
     @categories = Category.find(:all)
     @articles_newest   = Article.find(:all, :limit => 5, :order => 'created_at DESC')
@@ -23,7 +26,15 @@ protected
   end
   
   def allow_anonymous_access?
-	return Setting['plugin_redmine_knowledgebase']['knowledgebase_anonymous_access'].to_i == 1
+    Setting['plugin_redmine_knowledgebase']['knowledgebase_anonymous_access'].to_i == 1
+  end
+
+#######
+private
+#######
+
+  def force_404
+    render_404
   end
 
 end
