6ac77044f055393916e9f3943036daa9060e0b73
Alex Bevilacqua <alexbevi@gmail.com>
2012-01-02 07:29:14 -0500

    Fix knowledgebase routing due to changes in Redmine core
    svn r8162 removes the default mappings, so we need to explicitely define them

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.connect "/knowledgebase", :controller => "knowledgebase", :action => "index", :conditions => { :method => [:get] }
   map.resources :categories, :path_prefix => '/knowledgebase', :conditions => { :method => [:get, :post] } 
   map.resources :articles, :path_prefix => '/knowledgebase', :conditions => { :method => [:get, :post] }
 end
