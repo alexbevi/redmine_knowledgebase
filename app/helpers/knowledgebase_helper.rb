ffbe427340aa927e569f3c8e8da49676e97f462e
Alex Bevilacqua <alexbevi@gmail.com>
2012-02-14 05:52:27 -0500

    Roll the tag_cloud helper in the knowledgebase helper

--- app/helpers/knowledgebase_helper.rb
+++ app/helpers/knowledgebase_helper.rb
@@ -76,4 +76,14 @@ module KnowledgebaseHelper
     link_to(category.title,{ :controller => 'categories', :action => 'show', :id => category.id})
   end
   
+  def tag_cloud(tags, classes)
+    return if tags.empty?
+    
+    max_count = tags.sort_by(&:count).last.count.to_f
+    
+    tags.each do |tag|
+      index = ((tag.count / max_count) * (classes.size - 1)).round
+      yield tag, classes[index]
+    end
+  end
 end
