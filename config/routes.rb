cb5641cb0e8ce7865ae2a2dd0f86a89d0e14c7e6
Alex Bevilacqua <alexbevi@gmail.com>
2012-05-22 20:51:52 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect "/knowledgebase", :controller => "knowledgebase", :action => "index", :conditions => { :method => :get }
-  map.resources :categories, :path_prefix => '/knowledgebase', :conditions => { :method => [:get, :post] } 
-  map.resources :articles, :path_prefix => '/knowledgebase', :conditions => { :method => [:get, :post] }
-  map.with_options :controller => 'articles' do |article_routes|
-    article_routes.with_options :conditions => {:method => :get} do |actions|
-      actions.connect '/knowledgebase/articles/:id/rate', :action => 'rate'
-      actions.connect '/knowledgebase/articles/tagged', :action => 'tagged'
+match "/knowledgebase", :to => "knowledgebase#index", :via => :get
+scope "/knowledgebase" do
+  resources :categories, :via => [:get, :post]
+  resources :articles do
+    get "rate"
+    collection do
+      get "tagged"
+      post "preview"
     end
-    article_routes.with_options :conditions => {:method => :post} do |actions|
-      actions.connect '/knowledgebase/articles/preview', :action => 'preview'
-      actions.connect '/knowledgebase/articles/:id', :action => 'update'
-      actions.connect '/knowledgebase/articles/:id/comment', :action => 'comment'
-      actions.connect '/knowledgebase/articles/:id/add_comment', :action => 'add_comment'
-      actions.connect '/knowledgebase/articles/:id/destroy_comment', :action => 'destroy_comment'
-    end
+    post "comment"
+    post "add_comment"
+    post "destroy_comment"
   end
 end
