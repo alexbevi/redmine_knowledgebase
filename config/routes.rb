b08a420fdb55cc424c61615b7abe341723956634
Alex Bevilacqua <alexbevi@gmail.com>
2012-05-24 06:10:06 -0400

    fixes access to tagged articles for redmine 1.4.x

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ ActionController::Routing::Routes.draw do |map|
   map.connect "/knowledgebase", :controller => "knowledgebase", :action => "index", :conditions => { :method => :get }
   map.resources :categories, :path_prefix => '/knowledgebase', :conditions => { :method => [:get, :post] } 
   map.resources :articles, :path_prefix => '/knowledgebase', :conditions => { :method => [:get, :post] }
+  
   map.with_options :controller => 'articles' do |article_routes|
     article_routes.with_options :conditions => {:method => :get} do |actions|
       actions.connect '/knowledgebase/articles/:id/rate', :action => 'rate'
-      actions.connect '/knowledgebase/articles/tagged', :action => 'tagged'
+      actions.connect '/knowledgebase/articles/tagged/:id', :action => 'tagged'
     end
     article_routes.with_options :conditions => {:method => :post} do |actions|
       actions.connect '/knowledgebase/articles/preview', :action => 'preview'
