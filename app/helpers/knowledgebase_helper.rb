b9af081009f7a597243d3e4d96c5aa4cfc0f1299
Alex Bevilacqua <alexbevi@gmail.com>
2012-06-25 04:57:11 -0400

    fix a bug in the tag cloud builder if no tags are in use (fixes #82)

--- app/helpers/knowledgebase_helper.rb
+++ app/helpers/knowledgebase_helper.rb
@@ -79,9 +79,8 @@ module KnowledgebaseHelper
   
   def tag_cloud(tags, classes)
     return if tags.empty?
-    
     max_count = tags.sort_by(&:count).last.count.to_f
-    
+    return unless max_count > 0
     tags.each do |tag|
       index = ((tag.count / max_count) * (classes.size - 1)).round
       yield tag, classes[index]
