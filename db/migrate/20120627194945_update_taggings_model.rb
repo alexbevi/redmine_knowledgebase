8d786b8ed1dcf809ff5cd13a6300572ae300a694
Alex Bevilacqua <alexbevi@gmail.com>
2012-09-17 13:54:32 -0400

    updated migrations used for taggings >= 2.x

--- db/migrate/20120627194945_update_taggings_model.rb
+++ db/migrate/20120627194945_update_taggings_model.rb
@@ -1,10 +1,14 @@
 class UpdateTaggingsModel < ActiveRecord::Migration
   def change
-    remove_index :taggings, [:taggable_id, :taggable_type]
-    add_column :taggings, :tagger_id, :integer
-    add_column :taggings, :tagger_type, :string
-    add_column :taggings, :context, :string, :limit => 128
-    add_index :taggings, [:taggable_id, :taggable_type, :context]
-    ActsAsTaggableOn::Tagging.update_all(:context => "tags")
+    begin
+      remove_index :taggings, [:taggable_id, :taggable_type]
+      add_column :taggings, :tagger_id, :integer
+      add_column :taggings, :tagger_type, :string
+      add_column :taggings, :context, :string, :limit => 128
+      add_index :taggings, [:taggable_id, :taggable_type, :context]
+      ActsAsTaggableOn::Tagging.update_all(:context => "tags")
+    rescue
+      puts "UpdateTaggingsModel failed, so it probably wasn't necessary"
+    end
   end
 end
