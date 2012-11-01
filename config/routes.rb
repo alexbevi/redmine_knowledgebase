016ae4bc0318165805884b7281d61f934a6a252e
duanejevon <duanejevon@gmail.com>
2012-11-01 16:15:32 +0200

    Routes, AUTHORS and README updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 RedmineApp::Application.routes.draw do
 
-	match "/knowledgebase", :to => "knowledgebase#index", :via => :get
-
-	scope "/knowledgebase" do
-	  resources :categories, :via => [:get, :post]
-	  resources :articles do
-		collection do
-		  get "tagged"
-		  post "preview"
-		end
-		get "comment"
-		member do
-		  put  "preview"
-		  post "add_comment"
-		  post "destroy_comment"
-		  post "rate"
-		end
-	  end
+	resources :projects do
+  
+		match '/knowledgebase', :to => 'knowledgebase#index', :via => :get
+		
+		resources :categories, :via => [:get, :post]
+		resources :articles do
+				collection do
+				  get "tagged"
+				  post "preview"
+				  end
+				get "comment"
+				member do
+				  put  "preview"
+				  post "add_comment"
+				  post "destroy_comment"
+				  post "rate"
+				  end
+		end	
 	end
-
 end
