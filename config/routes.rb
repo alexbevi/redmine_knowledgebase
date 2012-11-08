7989478da138a83a5f638bd95da049db523d0488
Alex Bevilacqua <alexbevi@gmail.com>
2012-11-08 10:26:02 -0500

    fixed routing to properly use the /knowledgebase scope within a project

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 RedmineApp::Application.routes.draw do
-
-	resources :projects do
-  
-		match '/knowledgebase', :to => 'knowledgebase#index', :via => :get
-		
+	scope "/projects/:project_id/knowledgebase" do
+		get '/' => 'knowledgebase#index'
 		resources :categories, :via => [:get, :post]
 		resources :articles do
-				collection do
-				  get "tagged"
-				  post "preview"
-				  end
-				get "comment"
-				member do
-				  put  "preview"
-				  post "add_comment"
-				  post "destroy_comment"
-				  post "rate"
-				  end
-		end	
+			collection do
+			  get "tagged"
+			  post "preview"
+		  end
+			
+			get "comment"
+			
+			member do
+			  put  "preview"
+			  post "add_comment"
+			  post "destroy_comment"
+			  post "rate"
+		  end
+		end
 	end
 end
