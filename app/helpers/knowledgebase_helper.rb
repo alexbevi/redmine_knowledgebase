cd70ee84e0687a59fee9aec7cea095ae3b8451af
duanejevon <duanejevon@gmail.com>
2012-11-24 14:54:13 +0200

    Use sortable tables for various views

--- app/helpers/knowledgebase_helper.rb
+++ app/helpers/knowledgebase_helper.rb
@@ -38,4 +38,16 @@ module KnowledgebaseHelper
   def updated_by(updated, updater)
      l(:label_updated_who, :updater => link_to_user(updater), :age => time_tag(updated)).html_safe
   end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
 end
