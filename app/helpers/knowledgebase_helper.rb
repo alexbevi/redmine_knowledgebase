5f7626660734c1b3791d0096abc183c8653f4d5d
Alex Bevilacqua <alexbevi@gmail.com>
2013-08-21 13:48:54 -0400

    fix a logic error in the helper when creating remote_links

--- app/helpers/knowledgebase_helper.rb
+++ app/helpers/knowledgebase_helper.rb
@@ -53,7 +53,7 @@ module KnowledgebaseHelper
   def create_preview_link
     v = Redmine::VERSION.to_a
     if v[0] == 2 && v[1] <= 1
-      link_to_remote(l(:label_preview), { :url => { :controller => 'articles', :action => 'preview' }, :method => 'post', :update => 'preview', :with => "Form.serialize('articles-form')")
+      link_to_remote(l(:label_preview), :url => { :controller => 'articles', :action => 'preview' }, :method => 'post', :update => 'preview', :with => "Form.serialize('articles-form')")
     else
       preview_link({ :controller => 'articles', :action => 'preview' }, 'articles-form')
     end
