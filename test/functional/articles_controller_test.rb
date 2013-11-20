faa2cf3f8c1f9f9420ed3719763b16323fcd549a
Alex Bevilacqua <alexbevi@gmail.com>
2013-11-19 23:10:53 -0500

    passing ArticlesControllerTest#test_index

--- test/functional/articles_controller_test.rb
+++ test/functional/articles_controller_test.rb
@@ -1,8 +1,18 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects
+  plugin_fixtures :kb_articles
+
+  def setup
+    Project.find(1).enable_module! :knowledgebase
+  end
+
+  def test_index
+    @request.session[:user_id] = 1
+    get :index, :project_id => 1
+
+    assert_response :success
+    assert_template 'index'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+require "pry-nav"
 # Load the normal Rails helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
