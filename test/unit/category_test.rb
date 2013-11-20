0e7adedc9b6e06b4c8f967a2af765b2c866da92e
Alex Bevilacqua <alexbevi@gmail.com>
2013-11-20 09:52:41 -0500

    update fixtures for unit tests

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  fixtures :categories
+  plugin_fixtures :kb_categories
 
   # Replace this with your real tests.
   def test_truth
