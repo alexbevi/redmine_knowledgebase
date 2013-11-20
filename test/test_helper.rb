8614dd6472b1f03730f4207e087ca1501021e8ad
Alex Bevilacqua <alexbevi@gmail.com>
2013-11-20 10:08:14 -0500

    include plugin_fixtures patch for ActiveSupport::TestCase as well

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,3 +16,7 @@ end
 unless ActionController::TestCase.included_modules.include?(Redmine::PluginFixturesLoader)
   ActionController::TestCase.send :include, Redmine::PluginFixturesLoader
 end
+
+unless ActiveSupport::TestCase.included_modules.include?(Redmine::PluginFixturesLoader)
+  ActiveSupport::TestCase.send :include, Redmine::PluginFixturesLoader
+end
