ee4044f60f8da25ef1d3ba3b5bac658d8de93d0e
LabriePierre <labriepierre@gmail.com>
2014-03-11 11:35:53 -0400

    Update knowledgebase_helper.rb
    In the tagged view, the sort is not working, loosing his selection argument ( the tag) and not passing the sort argument. There is a modification also in the article_controler for the tagged method.

--- app/helpers/knowledgebase_helper.rb
+++ app/helpers/knowledgebase_helper.rb
@@ -54,7 +54,7 @@ module KnowledgebaseHelper
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+    link_to title, {:id => params[:id], :sort => column, :direction => direction }, {:class => css_class}
   end
 
   def article_to_pdf(article, project)
