a3dd3df411999889f57cf2de0b7a341f1c6febba
Alex Bevilacqua <alexbevi@gmail.com>
2014-03-18 08:48:55 -0400

    update version to 3.0.3

--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ Redmine::Plugin.register :redmine_knowledgebase do
   author_url  "http://www.alexbevi.com"
   description 'A plugin for Redmine that adds knowledgebase functionality'
   url         'https://github.com/alexbevi/redmine_knowledgebase'
-  version     '3.0.2'
+  version     '3.0.3'
 
   requires_redmine :version_or_higher => '2.0.0'
   requires_acts_as_taggable_on
